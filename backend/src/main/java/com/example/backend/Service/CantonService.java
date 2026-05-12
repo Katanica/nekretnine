c59919aec0e3d8e74995e8edc394005452f7659f
Jurica Stjepanovic <stjepanovic.jurica7@gmail.com>
2026-05-12 16:37:28 +0200

    Canton i City servisi

--- backend/src/main/java/com/example/backend/Service/CantonService.java
+++ backend/src/main/java/com/example/backend/Service/CantonService.java
@@ -1,8 +1,15 @@
 package com.example.backend.Service;
+import com.example.backend.DTO.CantonDto;
+import com.example.backend.Entity.Canton;
 import org.springframework.stereotype.Service;
 
 import com.example.backend.Repository.CantonRepository;
 
+import java.util.List;
+
 @Service
 public interface CantonService {
+    List<CantonDto> findAll();
+    Canton findById(Long profileId);
+
 }
--- backend/src/main/java/com/example/backend/Service/CityService.java
+++ backend/src/main/java/com/example/backend/Service/CityService.java
@@ -1,7 +1,15 @@
 package com.example.backend.Service;
+import com.example.backend.DTO.CityDto;
+import com.example.backend.Entity.City;
 import com.example.backend.Repository.CityRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public interface CityService {
+    List<CityDto> findAll();
+    City findById(Long id);
+    List<CityDto> getByCanton(Long cantonId);
+
 }
